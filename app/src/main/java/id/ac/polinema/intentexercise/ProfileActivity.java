8ab8aea2743b69576504ec9ecb226ba8980a4358
fildzahfsaa <fildfild12@gmail.com>
2020-02-27 20:18:07 +0700

    Intent-Exercise (DONE)

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,66 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView FullnameText;
+    private TextView EmailText;
+    private TextView HomepageText;
+    private TextView AboutText;
+    private ImageView ImageProfil;
+    String homepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        FullnameText = findViewById(R.id.label_fullname);
+        EmailText = findViewById(R.id.label_email);
+        HomepageText = findViewById(R.id.label_homepage);
+        AboutText = findViewById(R.id.label_about);
+        ImageProfil = findViewById(R.id.image_profile);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            String fullname = extras.getString(RegisterActivity.FULLNAME_KEY);
+            FullnameText.setText(fullname);
+            String email = extras.getString(RegisterActivity.EMAIL_KEY);
+            EmailText.setText(email);
+            homepage = extras.getString(RegisterActivity.HOMEPAGE_KEY);
+            HomepageText.setText(homepage);
+            String about = extras.getString(RegisterActivity.ABOUTYOU_KEY);
+            AboutText.setText(about);
+
+            Bitmap bitmap = (Bitmap) extras.get("Bitmap");
+            ImageProfil.setImageBitmap(bitmap);
+
+        }
+    }
+
+
+    public void homepageHandler(View view) {
+        Uri webpage = Uri.parse(homepage);
+        if (!homepage.startsWith("https://") && !homepage.startsWith("http://")){
+            homepage = "http://" + homepage;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -1,10 +1,19 @@
-package id.ac.polinema.intentexercise;
+package id .ac.polinema.intentexercise;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.mobsandgeeks.saripaar.ValidationError;
@@ -14,26 +23,39 @@ import com.mobsandgeeks.saripaar.annotation.Email;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class RegisterActivity extends AppCompatActivity implements Validator.ValidationListener {
 
+    private static final String TAG = RegisterActivity.class.getCanonicalName();
+    private static final int GALLERY_REQUEST_CODE = 1;
+
+    public static final String FULLNAME_KEY = "fullname";
+    public static final String EMAIL_KEY = "email";
+    public static final String PASSWORD_KEY = "password";
+    public static final String CONFIRMPASS_KEY = "confirmpass";
+    public static final String HOMEPAGE_KEY = "homepage";
+    public static final String ABOUTYOU_KEY = "aboutyou";
+
     @NotEmpty
     private EditText Fullname;
     @NotEmpty
-    @com.mobsandgeeks.saripaar.annotation.Email
+    @Email
     private EditText Email;
     @NotEmpty
-    @com.mobsandgeeks.saripaar.annotation.Password
+    @Password
     private EditText Password;
     @NotEmpty
-    @com.mobsandgeeks.saripaar.annotation.ConfirmPassword
+    @ConfirmPassword
     private EditText ConfirmPassword;
     @NotEmpty
     private EditText Homepage;
     @NotEmpty
     private EditText AboutYou;
     protected Validator validator;
+    private ImageView ImageProfil;
 
 
     @Override
@@ -43,12 +65,64 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
 
         validator = new Validator(this);
         validator.setValidationListener(this);
+
+        Fullname = findViewById(R.id.text_fullname);
+        Email = findViewById(R.id.text_email);
+        Password = findViewById(R.id.text_password);
+        ConfirmPassword = findViewById(R.id.text_confirm_password);
+        Homepage = findViewById(R.id.text_homepage);
+        AboutYou = findViewById(R.id.text_about);
+        ImageProfil = findViewById(R.id.image_profile);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE){
+            if (data != null){
+                try{
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    ImageProfil.setImageBitmap(bitmap);
+                }
+                catch (IOException e){
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
     }
 
 
     @Override
     public void onValidationSucceeded() {
 
+        String name = Fullname.getText().toString();
+        String email = Email.getText().toString();
+        String pass = Password.getText().toString();
+        String confpass = ConfirmPassword.getText().toString();
+        String home = Homepage.getText().toString();
+        String about = AboutYou.getText().toString();
+
+        ImageProfil.setDrawingCacheEnabled(true);
+        Bitmap b=ImageProfil.getDrawingCache();
+
+
+        Intent intent = new Intent(this, ProfileActivity.class);
+
+        intent.putExtra(FULLNAME_KEY, name);
+        intent.putExtra(EMAIL_KEY, email);
+        intent.putExtra(PASSWORD_KEY, pass);
+        intent.putExtra(CONFIRMPASS_KEY, confpass);
+        intent.putExtra(HOMEPAGE_KEY, home);
+        intent.putExtra(ABOUTYOU_KEY, about);
+        intent.putExtra("Bitmap", b);
+        startActivity(intent);
     }
 
     @Override
@@ -66,7 +140,14 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
         }
     }
 
-    public void handleOkRegister(View view) {
-        validator.validate(true);
+
+    public void handlePict(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
     }
+
+    public void okHandle(View view) {
+        validator.validate();
+    }
+
 }
